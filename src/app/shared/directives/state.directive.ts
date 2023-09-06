b6d9f5d70f4114bf7036f9e6ec2f3fe468427fe5
Sonia-Baibou <s.baibou@ohmycode.io>
2023-09-06 12:25:51 +0200

    directive appState

---
+++ src/app/shared/directives/state.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, HostBinding, Input } from '@angular/core';
+
+// DECORATOR
+@Directive({
+  selector: '[appState]',
+})
+export class StateDirective {
+  @Input() stateObj!: string;
+  @HostBinding('class') tdClassName!: string;
+
+  constructor() {
+    // console.log(this.stateObj, 'depuis constructor');
+  }
+
+  // déclenché une seule fois
+  ngOnInit() {
+    console.log(this.stateObj, 'depuis constructor');
+    // CONFIRMED state-confirmed
+    // this.tdClassName = 'state-' + this.stateObj.toLowerCase();
+    this.tdClassName = `state-${this.stateObj.toLowerCase()}`;
+  }
+
+  // CYCLE DE VIE - LIFE CYCLE
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TotalPipe } from './pipes/total.pipe';
+import { StateDirective } from './directives/state.directive';
 
 
 
 @NgModule({
   declarations: [
-    TotalPipe
+    TotalPipe,
+    StateDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    TotalPipe
+    TotalPipe,
+    StateDirective
   ]
 })
 export class SharedModule { }
